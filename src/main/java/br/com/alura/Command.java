99248f5a0473e0572a0741132a236e51ffe37676
Mateus Nascimento <mateus.nascimento@omnilink.com.br>
2024-09-04 11:45:14 -0300

    Aplica padrão command

---
+++ src/main/java/br/com/alura/Command.java
@@ -0,0 +1,8 @@
+package br.com.alura;
+
+/**
+ * @author Mateus Dantas
+ */
+public interface Command {
+    public void execute();
+}
---
+++ src/main/java/br/com/alura/CommandExecutor.java
@@ -0,0 +1,11 @@
+package br.com.alura;
+
+/**
+ * @author Mateus Dantas
+ */
+public class CommandExecutor {
+
+    public void executeCommand(Command command) {
+        command.execute();
+    }
+}
